1ee285a23f44bc009e4d0efe0366aca92a040e76
shabutora <tsukasa@iyher.net>
2017-01-26 06:57:41 +0900

    Delete git prompt.

--- cloudshell-setup.sh
+++ cloudshell-setup.sh
@@ -11,12 +11,3 @@ wget "https://raw.githubusercontent.com/Bash-it/bash-it/master/completion/availa
 export PATH=$PATH:$HOME/bin
 source ./terraform.completion.bash
 rm terraform.zip
-
-# git prompt
-wget "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh"
-source git-prompt.sh
-GIT_PS1_SHOWDIRTYSTATE=1
-GIT_PS1_SHOWUPSTREAM=1
-GIT_PS1_SHOWUNTRACKEDFILES=
-GIT_PS1_SHOWSTASHSTATE=1
-export PS1='\[\033[1;32m\]\u\[\033[00m\]:\[\033[1;34m\]\w\[\033[1;31m\]$(__git_ps1)\[\033[00m\] \$ '
