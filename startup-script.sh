d4c7d81ae7488a3e9cf41516cc49b0f719366824
shabutora <tsukasa@iyher.net>
2017-02-04 17:08:06 +0900

    Change docker install at once.

--- startup-script.sh
+++ startup-script.sh
@@ -10,14 +10,16 @@ if [ -d "$EX_DIR" ]; then
 fi
 
 # Install docker
-apt-get update && apt-get install -y  curl \
-                                      apt-transport-https \
-                                      ca-certificates \
-                                      software-properties-common
-curl -fsSL https://yum.dockerproject.org/gpg | apt-key add -
-sudo add-apt-repository \
-       "deb https://apt.dockerproject.org/repo/ \
-       debian-$(lsb_release -cs) \
-       main"
-apt-get update
-apt-get -y install docker-engine
+if [ -n "$(which docker)" ]; then
+  apt-get update && apt-get install -y  curl \
+                                        apt-transport-https \
+                                        ca-certificates \
+                                        software-properties-common
+  curl -fsSL https://yum.dockerproject.org/gpg | apt-key add -
+  sudo add-apt-repository \
+         "deb https://apt.dockerproject.org/repo/ \
+         debian-$(lsb_release -cs) \
+         main"
+  apt-get update
+  apt-get -y install docker-engine
+fi
