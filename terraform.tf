f202b790be80ad60af55d275b7d13b118b3e6440
shabutora <tsukasa@iyher.net>
2017-02-04 16:39:01 +0900

    Change attached disk order.

--- terraform.tf
+++ terraform.tf
@@ -23,7 +23,7 @@ resource "google_compute_instance" "handson_instance" {
     size = 10
   }
   disk {
-    disk = "${google_compute_disk.data_disk.self_link}"
+    disk = "${google_compute_disk.data_disk.name}"
     auto_delete = false
   }
   network_interface {
